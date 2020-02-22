cf745b43f0c91f022272ab4ce9f6796d0bf0b7f5
Douglas Campbell <dacampbelltbone@gmail.com>
2020-02-22 14:45:27 -0500

    Completed Ring Buffer and commented out Array Buffer tests. All Ring Buffer tests have passed.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,47 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < 0 or item is None:
+            return
+        elif self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            # if DLL length is 1, current is tail
+            if self.storage.length == 1:
+                self.current = self.storage.tail
+        else:
+            # if there is a next value on the current node
+            if self.current.next:
+                # store in variable
+                nxt_nde = self.current.next
+                # add to tail
+                self.storage.add_to_tail(item)
+                # if current equals head of DLL
+                if self.current == self.storage.head:
+                    # move tail to front of DLL
+                    self.storage.move_to_front(self.storage.tail)
+                else:
+                    # or insert tail value before
+                    self.current.insert_before(self.storage.tail.value)
+                    # increment DLL length
+                    self.storage.length += 1
+                    # delete DLL tail node
+                    self.storage.delete(self.storage.tail)
+                # delete current node
+                self.storage.delete(self.current)
+                # set next node as current
+                self.current = nxt_nde
+            else:
+                self.storage.add_to_tail(item)
+                self.storage.delete(self.current)
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
+        curr_nde = self.storage.head
+        while curr_nde:
+            list_buffer_contents.append(curr_nde.value)
+            curr_nde = curr_nde.next
 
         return list_buffer_contents
 
